3fb767a9e343f84f0acd66669ed8c8aa163e9601
udehlee <ritzeepita@gmail.com>
2025-04-24 13:22:53 +0100

    integrate bun ORM to my User struct for database operation

--- internals/models/models.go
+++ internals/models/models.go
@@ -1,17 +1,21 @@
 package models
 
+import "github.com/uptrace/bun"
+
 type User struct {
-	UserID    int    `json:"user_Id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Role      string `json:"role"`
-	Gender    string `json:"gender"`
-	Address   string `json:"address"`
+	bun.BaseModel `bun:"table:users"`
+
+	UserID    int64  `bun:",pk,autoincrement" json:"user_id"`
+	FirstName string `bun:"firstname" json:"firstname"`
+	LastName  string `bun:"lastname" json:"lastname"`
+	Email     string `bun:"email" json:"email"`
+	Password  string `bun:"password" json:"password"`
+	Role      string `bun:"role" json:"role"`
+	Gender    string `bun:"gender" json:"gender"`
+	Address   string `bun:"address" json:"address"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `bun:"email" json:"email"`
+	Password string `bun:"password" json:"password"`
 }
