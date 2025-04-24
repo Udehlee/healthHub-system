1955043dc37620cbc30b1aea96faa023be8158d3
udehlee <ritzeepita@gmail.com>
2025-04-24 13:24:27 +0100

    add HashPassword func to hash password before saving to DB

---
+++ utility/utils.go
@@ -0,0 +1,16 @@
+package utility
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+
+}
+
+func ComparePasswordHash(hashedPassword, plainPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+}
