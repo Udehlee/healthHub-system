61291e42e7e680b3b04919e812bf07f773ca99b5
udehlee <ritzeepita@gmail.com>
2025-04-27 11:16:27 +0100

    add admin user default details

---
+++ internals/db/seed.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"os"
+
+	"github.com/Udehlee/healthHub-System/internals/models"
+)
+
+func SeedData() models.User {
+	admin := models.User{
+		Email:    os.Getenv("ADMIN_EMAIL"),
+		Password: os.Getenv("ADMIN_PASSWORD"),
+	}
+
+	return admin
+
+}
