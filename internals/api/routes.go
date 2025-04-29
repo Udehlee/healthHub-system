98878db49b7ff85b3faa3bb24a7bca3ccd55186c
udehlee <ritzeepita@gmail.com>
2025-04-29 22:41:10 +0100

    add routes for admin and user

--- internals/api/routes.go
+++ internals/api/routes.go
@@ -1,10 +1,28 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	api "github.com/Udehlee/healthHub-System/internals/api/handlers"
+	"github.com/Udehlee/healthHub-System/middleware"
+	"github.com/gin-gonic/gin"
+)
 
-func Routes(r *gin.Engine, h *Handler) {
+func Routes(r *gin.Engine, h *api.Handler) {
 
 	r.POST("/api/register", h.Register)
 	r.POST("/api/login", h.Login)
 
+	//admin routes
+	adminRoutes := r.Group("/admin")
+	adminRoutes.Use(middleware.AuthMiddleware(), middleware.RoleMiddleware("admin"))
+	{
+		adminRoutes.POST("/assign-patient-to-staff", h.AdminAssign)
+		adminRoutes.GET("/Get-all-users", h.GetAllUsers)
+	}
+
+	//deafult user(patient)
+	userRoutes := r.Group("/user")
+	userRoutes.Use(middleware.AuthMiddleware(), middleware.RoleMiddleware("patient"))
+	{
+		userRoutes.POST("/book-appointment", h.BookAppointment)
+	}
 }
