63d6b69b66813e9b5877344d4f2691508b2d024e
udehlee <ritzeepita@gmail.com>
2025-04-30 16:42:53 +0100

    add appointments table schema

---
+++ internals/db/migrations/20250430131255_create_appointments_table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE appointments (
+    appointment_id BIGSERIAL PRIMARY KEY,
+    patient_id INT NOT NULL,
+    staff_id INT,
+    staff_role INT,
+    status_ VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    assigned_by INT,
+    FOREIGN KEY (patient_id) REFERENCES users(user_id),
+    FOREIGN KEY (staff_id) REFERENCES users(user_id),
+    FOREIGN KEY (staff_role) REFERENCES users(user_role),
+    FOREIGN KEY (assigned_by) REFERENCES users(user_id)
+);
