a519de1c0f27cc192abcc40ad05e09195534450f
udehlee <ritzeepita@gmail.com>
2025-04-30 16:45:48 +0100

    add appointment struct to handle appoinment details

--- internals/models/models.go
+++ internals/models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/golang-jwt/jwt"
 	"github.com/uptrace/bun"
 )
@@ -18,6 +20,20 @@ type User struct {
 	Address   string `bun:"address" json:"user_address"`
 }
 
+type Appointment struct {
+	AppointmentID int64     `bun:"appointment_id,pk,autoincrement"`
+	PatientID     int64     `bun:"patient_id"`
+	StaffID       *int64    `bun:"staff_id,nullzero"`
+	StaffRole     *int64    `bun:"staff_role,nullzero"`
+	Status        string    `bun:"status_"`
+	CreatedAt     time.Time `bun:"created_at,default:current_timestamp"`
+	AssignedBy    *int64    `bun:"assigned_by,nullzero"`
+}
+
+type AppointmentRequest struct {
+	PatientID int64  `bun:"patient_id" json:"patient_id"`
+	Status    string `bun:"status" json:"status"`
+}
 type LoginRequest struct {
 	Email    string `bun:"email" json:"email"`
 	Password string `bun:"password" json:"password"`
