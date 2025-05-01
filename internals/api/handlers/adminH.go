525ae61ad8ce8aa7e5814855aeae2544660f345f
udehlee <ritzeepita@gmail.com>
2025-05-01 20:15:06 +0100

    add AdminAssign func to assign staff to a booked appointment

--- internals/api/handlers/adminH.go
+++ internals/api/handlers/adminH.go
@@ -3,11 +3,40 @@ package api
 import (
 	"net/http"
 
+	"github.com/Udehlee/healthHub-System/internals/models"
+	"github.com/Udehlee/healthHub-System/utility"
 	"github.com/gin-gonic/gin"
 )
 
+// AdminAssign assigns a staff to a specific appointment.
 func (h *Handler) AdminAssign(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"error": "assigning staff jare!"})
+	appointmentID, err := utility.GetParamInt64(c, "appointment_id")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid appointment ID"})
+		return
+	}
+
+	var assignReq models.AssignRequest
+	if err := c.ShouldBindJSON(&assignReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to bind assign request"})
+		return
+	}
+
+	adminID := c.GetInt64("user_id")
+	appointment := &models.Appointment{
+		StaffID:    assignReq.StaffID,
+		StaffRole:  assignReq.StaffRole,
+		Status:     "assigned",
+		AssignedBy: &adminID,
+	}
+
+	err = h.Db.AssignStaff(appointmentID, appointment)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to assign staff"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "staff successfully assigned"})
 }
 
 // GetAllUsers retrieves all users
