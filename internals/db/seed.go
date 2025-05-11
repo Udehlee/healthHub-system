46e14ca90a2639bb1779c552c2e7bfb3cfa70fc9
udehlee <ritzeepita@gmail.com>
2025-05-11 09:57:45 +0100

    refactor seedData to hash admin password before storage

--- internals/db/seed.go
+++ internals/db/seed.go
@@ -1,18 +1,23 @@
 package db
 
 import (
+	"log"
 	"os"
 
 	"github.com/Udehlee/healthHub-System/internals/models"
+	"github.com/Udehlee/healthHub-System/utility"
 )
 
 func SeedData() models.User {
-	admin := models.User{
-		Email:    os.Getenv("ADMIN_EMAIL"),
-		Password: os.Getenv("ADMIN_PASSWORD"),
-		Role:     os.Getenv("ADMIN_ROLE"),
+	hashedPassword, err := utility.HashPassword(os.Getenv("ADMIN_PASSWORD"))
+	if err != nil {
+		log.Fatalf("Failed to hash admin password: %v", err)
 	}
 
+	admin := models.User{
+		Email:    os.Getenv("ADMIN_EMAIL"),
+		Password: hashedPassword,
+		Role:     os.Getenv("ADMIN_ROLE"),
+	}
 	return admin
-
 }
